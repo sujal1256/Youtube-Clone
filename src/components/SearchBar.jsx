723f4ec9d723c23858af069cbbfac50436650ddc
Sujal Malhotra <msujal29012004@gmail.com>
2024-09-25 17:38:54 +0530

    Deploy fix error

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -38,7 +38,7 @@ const SearchBar = () => {
     return () => {
       clearTimeout(timer);
     };
-  }, [searchText, getSearchSuggestions, store]);
+  }, [searchText, store]);
 
   return (
     <>
